912a92d5be177d9308a139c8548f100a29b20b64
dydybzh <evencindy@hotmail.fr>
2014-04-16 12:18:17 +0200

    ajout nouveau module

---
+++ src/com/example/metacog/EtreEmpathiqueII_Activity.java
@@ -0,0 +1,22 @@
+package com.example.metacog;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class EtreEmpathiqueII_Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_etre_empathique_2);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.etre_empathique_ii_, menu);
+		return true;
+	}
+
+}

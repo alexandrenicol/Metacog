73b4605660ba590fd189742dc7db962eb5146cb4
dydybzh <evencindy@hotmail.fr>
2014-04-16 12:21:26 +0200

    new act

--- src/com/example/metacog/EtreEmpathiqueII_Activity.java
+++ src/com/example/metacog/EtreEmpathiqueII_Activity.java
@@ -15,7 +15,7 @@ public class EtreEmpathiqueII_Activity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.etre_empathique_ii_, menu);
+		getMenuInflater().inflate(R.menu.etre_empathique_2_, menu);
 		return true;
 	}
 

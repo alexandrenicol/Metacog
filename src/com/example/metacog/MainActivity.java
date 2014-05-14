fda9277680a2a9a731f06fdf081f1fd80d205d2d
dydybzh <evencindy@hotmail.fr>
2014-05-14 16:54:41 +0200

    creation du fichier structure-module.xml au lancement

--- src/com/example/metacog/MainActivity.java
+++ src/com/example/metacog/MainActivity.java
@@ -1,6 +1,20 @@
 package com.example.metacog;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -14,23 +28,18 @@ public class MainActivity extends Activity
 	private Button utilisateur;
 	private Button admin;
 	private Button quit;
+	
+	private String externalStorage;
+	private String structureFilename;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		File theDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Metacog");  // Defining Directory/Folder Name  
-		try{   
-		    if (!theDir.exists()){  // Checks that Directory/Folder Doesn't Exists!  
-		    	boolean result = theDir.mkdir();    
-		    	if(result){  
-		    	//JOptionPane.showMessageDialog(null, "New Folder created!");
-		    	}
-		    }
-		}catch(Exception e){  
-		    //JOptionPane.showMessageDialog(null, e);  
-		}
+		createMetacogFolder();
+		
+		createStructureModuleXML();
 		
 		utilisateur=(Button) findViewById(R.id.activity_main_utilisateur);
         utilisateur.setOnClickListener(new View.OnClickListener() {
@@ -70,4 +79,66 @@ public class MainActivity extends Activity
 		return true;
 	}
 */
+	
+	protected void createMetacogFolder(){
+		File theDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Metacog");  // Defining Directory/Folder Name  
+		try{   
+		    if (!theDir.exists()){  // Checks that Directory/Folder Doesn't Exists!  
+		    	boolean result = theDir.mkdir();    
+		    	if(result){  
+		    	//JOptionPane.showMessageDialog(null, "New Folder created!");
+		    	}
+		    }
+		}catch(Exception e){  
+		    //JOptionPane.showMessageDialog(null, e);  
+		}
+	}
+	
+	protected void createStructureModuleXML(){
+		externalStorage = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Metacog";
+        structureFilename = externalStorage+"/structure_modules.xml";
+        
+        InputStream is = getResources().openRawResource(R.raw.modules);
+        
+        File f =  new File(structureFilename);
+    	if (!f.exists()){
+    		try {
+    			//lecture du fichier xml 
+				Document xml = Utils.readXml(is);
+				
+				// write the content into xml file
+				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+				Transformer transformer = transformerFactory.newTransformer();
+				DOMSource source = new DOMSource(xml);
+				StreamResult result = new StreamResult(new File(structureFilename));
+		 
+				// Output to console for testing
+				// StreamResult result = new StreamResult(System.out);
+		 
+				transformer.transform(source, result);
+		 
+				//System.out.println("File saved!");
+				
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransformerConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransformerFactoryConfigurationError e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TransformerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+    	}
+	}
 }

4b9ddffe36b01fb09caa0526b5975018f2a8367b
alexandrenicol <nicol.alexandre1@gmail.com>
2014-06-04 14:20:29 +0200

    correction finish

--- src/com/example/metacog/AdminModuleActivity.java
+++ src/com/example/metacog/AdminModuleActivity.java
@@ -235,7 +235,6 @@ public class AdminModuleActivity extends Activity {
         		third.putExtra("selectedSerieName", "None");
         		third.putExtra("type", "Serie");
         		startActivity(third);
-        		finish();
 	    	}
 	    });
         
